3f6878b38049cace426bdf0fdf407b4d05ebccd5
stephantul <stephan.tulkens@uantwerpen.be>
2018-10-18 12:10:39 +0200

    fix tests

--- test_wubi.py
+++ test_wubi.py
@@ -6,30 +6,30 @@ import wubi
 class BasicTestSuite(unittest.TestCase):
     """Basic test cases."""
     def test_chinese_with_english1(self):
-        self.assertEqual(wubi.get('Wen Chao Wang 爱自由', 'cw'),
+        self.assertEqual(wubi.to_wubi('Wen Chao Wang 爱自由'),
                          'WenChaoWang ep thd mh')
 
     def test_chinese_with_english2(self):
-        self.assertEqual(wubi.get('WenChao Wang 爱自由', 'cw'),
+        self.assertEqual(wubi.to_wubi('WenChao Wang 爱自由'),
                          'WenChaoWang ep thd mh')
 
     def test_chinese_with_english3(self):
-        self.assertEqual(wubi.get('WenChaoWang 爱自由', 'cw'),
+        self.assertEqual(wubi.to_wubi('WenChaoWang 爱自由'),
                          'WenChaoWang ep thd mh')
 
     def test_chinese_with_english4(self):
-        self.assertEqual(wubi.get('爱wang', 'cw'), 'ep wang')
+        self.assertEqual(wubi.to_wubi('爱wang'), 'ep wang')
 
     def test_wibi_with_english1(self):
-        self.assertEqual(wubi.get('WenChaoWang ep thd mh', 'wc'),
+        self.assertEqual(wubi.from_wubi('WenChaoWang ep thd mh'),
                          'WenChaoWang爱自由')
 
     def test_wubi_with_english2(self):
-        self.assertEqual(wubi.get('WenChaoWang ep thd mh', 'wc'),
+        self.assertEqual(wubi.from_wubi('WenChaoWang ep thd mh'),
                          'WenChaoWang爱自由')
 
     def test_wubi_with_english3(self):
-        self.assertEqual(wubi.get('WenChaoWang ep thd mh', 'wc'),
+        self.assertEqual(wubi.from_wubi('WenChaoWang ep thd mh'),
                          'WenChaoWang爱自由')
 
 
