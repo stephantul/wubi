6dec3b02e48eeb11cdd3621c8a31482798235f1c
stephantul <stephan.tulkens@uantwerpen.be>
2018-10-18 13:04:11 +0200

    streamline code

--- wubi/wubi.py
+++ wubi/wubi.py
@@ -1,58 +1,54 @@
 # -*- coding: utf-8 -*-
-from string import ascii_letters
-from wubi.cw import cw
-from wubi.wc import wc
+from string import ascii_letters, punctuation
+from .cw import cw
+from .wc import wc
+
+roman = var = ascii_letters + '0123456789' + punctuation
 
 
-# init wubi dict
-data = {}
-data['cw'] = cw
-data['wc'] = wc
-
-
-def _wubi_generator(chars):
+def _wubi_generator(characters):
     """Generate wubi for chars, if char is not chinese character,
     itself will be returned.
     Chars must be unicode list.
     """
-    s = []
-    for char in chars:
-        # handle english in chinese
-        var = ascii_letters
-        if char == ' ':
-            pass
-        elif char in var:
-                s.append(char)
-                if char == chars[-1]:
-                    yield ''.join(s)
+    buffer = []
+    for char in characters:
+        # handle roman characters in chinese
+        if char == " ":
+            buffer.append(" ")
+        # If we encounter a roman character, add it to the buffer
+        elif char in roman:
+            buffer.append(char)
         else:
-            if len(s) != 0:
-                yield ''.join(s)
-                s = []
-            wubi = data['cw'].get(char)
-            if wubi is None:
-                wubi = char
-            yield wubi
+            # If the buffer is not empty
+            # yield the buffer, set the buffer to 0
+            if len(buffer) != 0:
+                yield "".join(buffer)
+                buffer = []
+            yield cw.get(char, char)
+    else:
+        # Yield the buffer at the end.
+        yield "".join(buffer)
 
 
-def _chinese_generator(chars, delimiter):
+def _chinese_generator(characters, delimiter):
     """Generate chinese for chars, if char is not chinese character,
     itself will be returned.
     Chars must be unicode list.
     """
-    for char in chars.split(delimiter):
-        yield data['wc'].get(char, char)
+    for char in characters.split(delimiter):
+        yield wc.get(char, char)
 
 
 def to_wubi(s, delimiter=" ", dictionary=None):
     """Translate chinese characters to wubi encoding."""
     if dictionary is not None:
-        data['cw'].update(dictionary)
+        cw.update(dictionary)
     return delimiter.join(_wubi_generator(s))
 
 
 def from_wubi(s, delimiter=" ", dictionary=None):
     """Translate wubi encoded characters to chinese."""
     if dictionary is not None:
-        data['cw'].update(dictionary)
-    return ''.join(_chinese_generator(s, delimiter))
+        wc.update(dictionary)
+    return "".join(_chinese_generator(s, delimiter))
